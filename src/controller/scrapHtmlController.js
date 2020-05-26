7fdeb663fcc86087fdb64afb5762d1ec15ff0e80
SunitaBlustack <sunita.kumari@bluestacks.com>
2020-05-26 20:24:14 +0530

    finding title and adding heading/content to section done

--- src/controller/scrapHtmlController.js
+++ src/controller/scrapHtmlController.js
@@ -1,31 +1,74 @@
+/*  
+
+logic: based on tags
+h1 -> titles
+h2 -> section heading
+Content between h2 comes as section content
+
+*/
+
+
 import rp from 'request-promise';
 import $ from 'cheerio';
 
+$.prototype.between = function (elm0, elm1) {
+    var index0 = $(this).index(elm0);
+    var index1 = $(this).index(elm1);
+    if (index0 <= index1)
+        return this.slice(index0, index1 + 1);
+    else
+        return this.slice(index1, index0 + 1);
+}
+
 const scrapHTML = (request, response, next) => {
-    console.log(request.body);
     const { url } = request.body;
     rp(url).then((html)=> {
+
+        const output = {};
+        const title = $('h1', html).eq(0).text();
+        const sectionHeadings = $('h2', html);
         
-        const wikiUrls = [];
-        for (let i = 0; i < 45; i++) {
-            wikiUrls.push($('b > a', html)[i].attribs.href);
+
+        output.sections= [];
+        for(let i=0;i<sectionHeadings.length;i++) {
+            output.sections.push({
+                'heading': sectionHeadings.eq(i).text()
+            });
         }
-    
-        return Promise.all(
-            wikiUrls.map(function(itemUrl) {
-                return rp('https://en.wikipedia.org' + itemUrl).then((html)=> {
-                    return {
-                        name: $('.firstHeading', html).text(),
-                        bday: $('.bday', html).text()
-                    };
-                }).catch((err)=>{
-                    response.json(err);
+        const breakHtmIntoSection = [];
+
+        const heading2Section = $('h2', html).nextUntil('h2').addBack();
+        heading2Section.each((index, value) => {
+            if(value.type === 'tag' && value.name === 'h2') {
+                breakHtmIntoSection.push(index);
+            }
+        });
+        const sectionHTML = [];
+        if(breakHtmIntoSection && breakHtmIntoSection.length) {
+            for(let i=0;i<breakHtmIntoSection.length;i++) {
+                sectionHTML[i] = [];
+                heading2Section.each((index, value) => {
+                    if(i+1 < breakHtmIntoSection.length && index === breakHtmIntoSection[i+1]) {
+                        index++;
+                        return false;
+                    } else {
+                        const textContent = heading2Section.eq(index).text();
+                        if(textContent) {
+                            sectionHTML[i].push(textContent);
+                        }
+                        
+                    }
                 });
-            })
-        );
-    }).then((presidents)=>{
-        response.json(presidents)
-        // console.log(presidents);
+                output.sections[i] = {
+                    ...output.sections[i],
+                    'content': sectionHTML[i],
+                }
+            }
+        }
+        output.title = title;
+        return output;
+    }).then((blogContent)=>{
+        response.json(blogContent)
     }).catch((err)=>{
         response.json(err);
     });
--- src/testWithBrowser.js
+++ src/testWithBrowser.js
@@ -3,29 +3,32 @@ const url = 'https://www.reddit.com';
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 
-/* rp(url)
-.then(function(html){
-//success!
-console.log(html);
-})
-.catch(function(err){
-//handle error
-console.log('error', err);
-}); */
 
 puppeteer
-    .launch()
-    .then(function(browser){
-        return browser.newPage();
-    })
-    .then(function(page){
-        return page.goto(url).then(function(){
-            return page.content();
-        }).then(function(html) {
-            $('h2', html).each(function() {
-              console.log($(this).text());
-            });
-          }).catch(function(err){
-            console.log('error', err);
+.launch()
+.then(function(browser){
+    console.log('browser');
+    return browser.newPage();
+})
+.then(function(page){
+    return page.goto(url).then(function(){
+        return page.content();
+    }).then(function(html) {
+        $('h2', html).each(function() {
+            console.log($(this).text());
         });
+        }).catch(function(err){
+        console.log('error', err);
     });
+});
+
+/* async function getPic() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://google.com');
+  await page.screenshot({path: 'google.png'});
+
+  await browser.close();
+}
+
+getPic(); */
